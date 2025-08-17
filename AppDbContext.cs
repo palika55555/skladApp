73a63756ddf63cd0325638005593b8c3425eea67
palika55555 <60897823+palika55555@users.noreply.github.com>
2025-08-17 16:47:41 +0200

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkladApi.Models;
+using System.Collections.Generic;
+
+namespace SkladApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.CustomerName)
+                      .HasColumnName("customer_name");
+                entity.Property(e => e.CreatedAt)
+                      .HasColumnName("created_at");
+            });
+
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(oi => oi.Product)
+                .WithMany(p => p.OrderItems)
+                .HasForeignKey(oi => oi.ProductId);
+
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(oi => oi.Order)
+                .WithMany(o => o.OrderItems)
+                .HasForeignKey(oi => oi.OrderId);
+        }
+    }
+}
